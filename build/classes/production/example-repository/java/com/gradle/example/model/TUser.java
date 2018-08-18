7bc4672c990ac5a6a4403e5b88864b92f815803d
wangqing <vic_q@sina.cn>
2018-08-18 19:10:40 +0800

    首次提交

---
+++ build/classes/production/example-repository/java/com/gradle/example/model/TUser.java
@@ -0,0 +1,53 @@
+package com.gradle.example.model;
+
+public class TUser {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer createAt;
+
+    private Integer updateAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Integer createAt) {
+        this.createAt = createAt;
+    }
+
+    public Integer getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Integer updateAt) {
+        this.updateAt = updateAt;
+    }
+}
